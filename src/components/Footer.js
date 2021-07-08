ad045fef4246553a3f6ea47f18d82c57adb67049
Josh Wayne <josh@joshwayne.com>
2021-07-07 23:22:19 -0600

    Footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,111 +1,60 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../assets/img/logo.svg'
-import facebook from '../assets/img/social/facebook.svg'
-import instagram from '../assets/img/social/instagram.svg'
-import twitter from '../assets/img/social/twitter.svg'
-import vimeo from '../assets/img/social/vimeo.svg'
+import logo from "../assets/img/logo-dark.svg";
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div style={{ maxWidth: '100vw' }} className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
-            </div>
+      <footer className="footer">
+        <div class="footer--content container">
+          <div className="footer--logo-column">
+            <Link className="logo" to="/">
+              <img className="logo--icon" src={logo} alt="Logo" />
+              <span className="logo--text">Uncommon sense</span>
+            </Link>
           </div>
+          <div className="footer--nav-column">
+            <Link to="/patterns">
+              Design Patterns
+            </Link>
+            <Link to="/page-designs">
+              Page Designs
+            </Link>
+            <Link className="navbar--item" to="/blog">
+                Blog
+              </Link>
+              <Link className="navbar--item" to="/about">
+                About
+              </Link>
+              <Link className="navbar--item" to="/sign-in">
+                Sign in
+              </Link>
+          </div>
+          <div className="footer--nav-column">
+          <Link to="/contact">
+              Contact
+            </Link>
+            <Link to="/uses">
+              What do you use?
+            </Link>
+            <Link to="/sitemap">
+              Sitemap
+            </Link>
+            <Link to="/disclaimer">
+              Disclaimer
+            </Link>
+          </div>
+          <div className="footer--newsletter">
+            <form action="">
+              <input className="footer--input" type="text" name="name" id="name" placeholder="Name" />
+              <input className="footer--input" type="email" name="email" id="email" placeholder="Email" />
+              <input className="footer--button" type="button" value="Sign up" />
+            </form>
+          </div>
+        </div>
+        <div className="footer--legal">
+          You look nice today. © 2021 Uncommon Sense. All rights reserved.
         </div>
       </footer>
     )
