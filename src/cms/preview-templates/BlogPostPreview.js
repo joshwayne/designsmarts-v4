c7fc0c71c3fa844ab635dcd5563ff62caf19a5c4
Josh Wayne <josh@joshwayne.com>
2021-07-16 23:52:02 -0600

    data preview WIP

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,26 +1,36 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { BlogPostTemplate } from "../../templates/blog-post";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { BlogPostTemplate } from '../../templates/blog-post'
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
-  // const tags = entry.getIn(['data', 'tags'])
+  const content = { __html: widgetFor('body') }
+  const title = entry.getIn(['data', 'title'])
+  const description = entry.getIn(['data', 'description'])
+  const tags = entry.getIn(['data', 'tags'])
+
+
+  console.log(entry);
+  console.log("Title: " + entry.getIn(['data', 'title']));
+  console.log("Description: " + entry.getIn(['data', 'description']));
+  console.log(content);
+  console.log("Tags: " + entry.getIn(['data', 'tags']));
+
   return (
-    <BlogPostTemplate
-    // content={widgetFor('body')}
-    // description={entry.getIn(['data', 'description'])}
-    // tags={tags && tags.toJS()}
-    // title={entry.getIn(['data', 'title'])}
-    >
-      <p>This is my new article.</p>
-    </BlogPostTemplate>
-  );
-};
+    <div>
+      Hello. 
+      {title}
+      {description}
+      <section dangerouslySetInnerHTML={content.body} />
+      {tags}
+    </div>
+  )
+}
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
-};
+}
 
-export default BlogPostPreview;
+export default BlogPostPreview
